935dc54f169a47160d58b21405abd25169c4d141
Aruzhan <zhumabaiaruzhan0@gmail.com>
2026-05-17 21:33:41 +0500

    Added research paper management system

--- src/kz/university/Main.java
+++ src/kz/university/Main.java
@@ -74,17 +74,50 @@ public class Main {
         report.print();
 
         ResearchPaper p1 = new ResearchPaper("P1", "AI in Education", "IEEE Access", "10.1109/ACCESS.2025.11368348", 10, 45, LocalDate.of(2026, 2, 15));
+        p1.addAuthor(teacher);
+        p1.addAuthor(student);
         ResearchPaper p2 = new ResearchPaper("P2", "Cybersecurity in University Systems", "Springer Journal", "10.1007/S001.2025.000002", 16, 20, LocalDate.of(2025, 11, 10));
+        p2.addAuthor(researcher);
+        ResearchPaper p3 = new ResearchPaper("P3", "LMS Logs and Student Performance: The Influence of Retaking a Course", "IEEE Access", "10.1109/ACCESS.2022.3169978", 14, 120, LocalDate.of(2022, 5, 2));
+        p3.addAuthor(teacher);
+        p3.addAuthor(researcher);
+        ResearchPaper p4 = new ResearchPaper("P4", "Artificial Intelligence for Smart Campus Management", "Springer Journal of Educational Technology", "10.1007/S001.2025.000004", 18, 76, LocalDate.of(2025, 9, 18));
+        p4.addAuthor(researcher);
+        ResearchPaper p5 = new ResearchPaper("P5", "Cybersecurity and Cloud Infrastructure in Modern Universities", "IEEE Transactions on Cloud Computing", "10.1109/TCC.2024.000005", 21, 94, LocalDate.of(2024, 12, 7));
+        p5.addAuthor(teacher);
+        p5.addAuthor(student);
+        ResearchPaper p6 = new ResearchPaper("P6", "Big Data Analytics in Higher Education", "Elsevier Future Generation Computer Systems", "10.1016/J.FUTURE.2025.000006", 25, 132, LocalDate.of(2025, 6, 14));
+        p6.addAuthor(teacher);
+        p6.addAuthor(researcher);
+        ResearchPaper p7 = new ResearchPaper("P7", "IoT-Based Smart Classroom Monitoring System", "IEEE Internet of Things Journal", "10.1109/JIOT.2024.000007", 19, 88, LocalDate.of(2024, 8, 30));
+
+        p7.addAuthor(student);
+        p7.addAuthor(researcher);
 
         teacher.publishPaper(p1);
+        teacher.publishPaper(p3);
+        teacher.publishPaper(p5);
+        teacher.publishPaper(p6);
+
         researcher.publishPaper(p2);
+        researcher.publishPaper(p3);
+        researcher.publishPaper(p4);
+        researcher.publishPaper(p6);
+        researcher.publishPaper(p7);
+
         student.publishPaper(p1);
+        student.publishPaper(p7);
 
         ResearchProject project = new ResearchProject("RP1", "Smart Campus", "ACTIVE");
         teacher.joinProject(project);
         researcher.joinProject(project);
         project.addPaper(p1);
         project.addPaper(p2);
+        project.addPaper(p3);
+        project.addPaper(p4);
+        project.addPaper(p5);
+        project.addPaper(p6);
+        project.addPaper(p7);
 
         ResearchService researchService = new ResearchService();
         researchService.addResearcher(student);
