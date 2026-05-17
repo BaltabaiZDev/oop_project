0ef3b84c18ff44058856c03c3fdc83380970ba07
BaltabaiZDev <baltabayzhanibek7@gmail.com>
2026-05-17 17:14:49 +0500

    put mark

---
+++ src/kz/university/exceptions/IllegalMarkException.java
@@ -0,0 +1,7 @@
+package kz.university.exceptions;
+
+public class IllegalMarkException extends RuntimeException {
+    public IllegalMarkException(String message) {
+        super(message);
+    }
+}
